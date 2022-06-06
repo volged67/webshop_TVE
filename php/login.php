fb2826b8d1e180b4b9f4043bd17e893ed0fefaba
volged67 <vg67@hotmail.de>
2022-06-06 17:31:23 +0200

    login navbar eingefügt

--- php/login.php
+++ php/login.php
@@ -11,6 +11,13 @@
 
 </head>
 
+
+
+<?php 
+// Navigationsleiste
+    include 'navbar.php';
+?>
+
 <body>
     <div id="login">
         <h3 class="text-center text-white pt-5">Login form</h3>
@@ -20,19 +27,21 @@
                     <div id="login-box" class="col-md-12">
                         <form id="login-form" class="form" action="checklogin.php" method="post">
                             <h3 class="text-center text-info">Login</h3>
+                            <!--Mail Adresse Eingabefeld-->
                             <div class="form-group">
                                 <label for="email" class="text-info">e-Mail</label><br>
                                 <input type="mail" name="email" class="form-control">
                             </div> 
+                            <!--Passwort Eingabefeld-->
                              <div class="form-group">
                                 <label for="password" class="text-info">Passwort:</label><br>
                                 <input type="password" name="password" id="password" class="form-control">
                             </div>
-                            
+                            <!--Konto erstellen Button-->
                             <div class="form-group">
                                 <input type="submit" value="Login" id="login" class="btn btn-primary">
                             </div>    
-                                         
+                            <!--Registrierungslink-->       
                             <div id="register-link" class="text-right">
                                 <a href="registrierung.php" class="text-info">Registrieren</a>
                             </div>
